7616ea9aed3427f33b8184d5201dd9ec73a0eb54
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2012-12-06 08:38:52 +0100

    Update project prerequsities and metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("USOS API Browser")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
